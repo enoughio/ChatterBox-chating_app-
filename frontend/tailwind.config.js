4463ecd9a0722a015d244c04e4784b4a5fe8c1f9
Ani <turboji007@gmail.com>
2024-05-16 19:37:03 +0530

    setup tailwind and daisyui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+    //...
+    plugins: [
+      require('daisyui'),
+    ],
+}
